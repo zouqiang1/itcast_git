7c3ed047aa418ae7c9bb43a00568aa2b41ec1aff
admin <842473809@qq.com>
2019-07-27 13:51:19 +0800

    分支

--- itheima/src/main/java/pojo/fun.java
+++ itheima/src/main/java/pojo/fun.java
@@ -2,6 +2,9 @@ package pojo;
 
 public class fun {
     public static void main(String[] args) {
+        System.out.println(22);
         System.out.println(11);
+        System.out.println(33);
+        System.out.println(3333);
     }
 }
